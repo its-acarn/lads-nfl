47a7f57bf79fc7881089f1bcbd7f80cd2f96e080
its-acarn <andrew.carnaghan@walksafe.io>
2023-10-23 18:10:51 +0100

    Add new league year dynasty

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,5 @@
 export const ladsLeagueId2022 = '798545046239588352'
+export const ladsLeagueId2023 = '992218083563216896'
 export const dynastyLeagueId2022 = '798543503410966528'
 export const dynastyLeagueId2023 = '927733787578949632'
 
