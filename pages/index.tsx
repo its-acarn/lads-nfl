42d69545cf2959577bb6dc1d470513bcfa1822ac
its-acarn <andrew.carnaghan@walksafe.io>
2023-10-23 18:16:05 +0100

    Update LLL code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
 import { ChangeEvent, useState } from 'react';
 import type { NextPage } from 'next'
-import styled from 'styled-components'
-// import { Input } from 'antd';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { addRosters, addLeagueDetails } from '../redux/leagueDetailsSlice';
 import { useRouter } from 'next/router';
-import { dynastyLeagueId2022, dynastyLeagueId2023, ladsLeagueId2022 } from '../config/config';
-import { Text, Button, Flex, Input, VStack, HStack } from '@chakra-ui/react';
+import {  dynastyLeagueId2023, ladsLeagueId2022, ladsLeagueId2023 } from '../config/config';
+import { Text, Button, Input, VStack, HStack } from '@chakra-ui/react';
 import { getAllPreviousLeagueDetails } from '../helpers/getAllPreviousLeagueDetails';
 
 const Home: NextPage = () => {
-  const [error, setError] = useState<boolean>(false)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [leagueId, setLeagueID] = useState<string>('')
-  const dispatch = useDispatch()
   const router = useRouter()
+  const dispatch = useDispatch()
+  const [error, setError] = useState<boolean>(false)
+  const [leagueId, setLeagueID] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const onSearch = async (id: string) => {
     setIsLoading(true)
@@ -66,18 +64,23 @@ const Home: NextPage = () => {
     setLeagueID(dynastyLeagueId2023)
   }
 
+  const onLads2023Click = () => {
+    setError(false)
+    setLeagueID(ladsLeagueId2023)
+  }
+
   return (
     <VStack bg={'quinary'} roundedTop={'lg'} py={8} spacing={5} h={'100vh'}>
       <Text color={'primary'} fontWeight={600}>Enter your Sleeper League ID:</Text>
       <VStack>
         <HStack>
-          <Input value={leagueId} rounded={'full'} bg={'primary'} color={'quinary'} placeholder="e.g. 798545046239588352" onChange={onChange} />
+          <Input value={leagueId} w={'60vw'} rounded={'full'} bg={'primary'} border={'none'} color={'quinary'} placeholder="e.g. 798545046239588352" onChange={onChange} />
           <Button rounded={'full'} onClick={() => onSearch(leagueId)} isLoading={isLoading} variant={'outline'} color={'primary'}>Go</Button>
         </HStack>
         {error && <Text color={'red.500'}>No league found for this ID</Text>}
       </VStack>
       <HStack>
-        <Button size={'sm'} bg={'quaternary'} color={'primary'} onClick={onLadsClick}>LadsLadsLads</Button>
+        <Button size={'sm'} bg={'quaternary'} color={'primary'} onClick={onLads2023Click}>LadsLadsLads</Button>
         <Button size={'sm'} bg={'quaternary'} color={'primary'} onClick={onDynasty2023Click}>Dynasty</Button>
       </HStack>
     </VStack>
